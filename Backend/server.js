e56679ccf1376965514aa2bec9f6c29548d1a5be
gksupertech6 <gksupertech6@gmail.com>
2026-06-12 20:27:49 +0530

    Fix Render port

--- Backend/server.js
+++ Backend/server.js
@@ -12,6 +12,8 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 
-app.listen(5000, () => {
-  console.log("Server running on 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
 });
