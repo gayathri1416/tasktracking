c6a3909f62d27b0e01cfc9af751a0dbe96e6af3d
gksupertech6 <gksupertech6@gmail.com>
2026-06-13 19:38:44 +0530

    loginpage

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -8,9 +8,10 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Register />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
 
-        <Route path="/login" element={<Login />} />
+        
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/tasks" element={<Tasks />} />
       </Routes>
