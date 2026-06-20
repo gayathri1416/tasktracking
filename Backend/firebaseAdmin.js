86daef36802bb8e0ec250d5df252c0c2387ad643
gksupertech6 <gksupertech6@gmail.com>
2026-06-20 22:47:30 +0530

    Use Firebase env variables on Railways

--- Backend/firebaseAdmin.js
+++ Backend/firebaseAdmin.js
@@ -1,4 +1,13 @@
+console.log("PROJECT_ID:", !!process.env.FIREBASE_PROJECT_ID);
+console.log("CLIENT_EMAIL:", !!process.env.FIREBASE_CLIENT_EMAIL);
+console.log("PRIVATE_KEY:", !!process.env.FIREBASE_PRIVATE_KEY);
 const { initializeApp, cert, getApps } = require("firebase-admin/app");
+
+const privateKey = process.env.FIREBASE_PRIVATE_KEY;
+
+if (!privateKey) {
+  throw new Error("FIREBASE_PRIVATE_KEY is missing");
+}
 
 const app =
   getApps().length === 0
@@ -6,7 +15,7 @@ const app =
         credential: cert({
           projectId: process.env.FIREBASE_PROJECT_ID,
           clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-          privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+          privateKey: privateKey.replace(/\\n/g, "\n"),
         }),
       })
     : getApps()[0];
